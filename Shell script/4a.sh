4ab5b402bb544d621e473c80a974e7e499239cf9
Geethanjaliks001 <100342019+Geethanjaliks001@users.noreply.github.com>
2022-02-24 21:43:14 +0530

    Update 4a.sh

--- Shell script/4a.sh
+++ Shell script/4a.sh
@@ -1,5 +1,5 @@
-#Write a shell script that accept one or more file names as argument and convert all of them
-to uppercase, provided they exists in current directory#
+#4a.Write a shell script that accept one or more file names as argument and convert all of them
+#to uppercase, provided they exists in current directory#
 for i in $* if [ -f $i ]
 do
 echo "----$i contents are-------"
